001179545e4dc07909b3e17ee1c2d9cb325b998a
clxisclx <1075549524@qq.com>
2024-01-31 00:29:46 +0800

    feat: sku列表

---
+++ src/api/product/type.ts
@@ -0,0 +1,55 @@
+﻿export interface ResponseData {
+    code: number
+    message: string
+    ok: boolean
+}
+
+// sku对象的类型
+export interface Attr {
+    id?: number
+    attrId: number|string // 平台属性的id
+    valueId: number|string // 属性值的id
+}
+
+export interface saleArr {
+    id?: number
+    saleAttrId: number | string // 属性id
+    saleAttrValueId: number | string // 属性值的id
+}
+
+export interface SkuData {
+    category3Id?: string | number // 三级分类的id
+    spuId?: string | number // 已有的SPU的ID
+    tmId?: string | number // SPU品牌的ID
+    skuName: string // sku名字
+    price?: string | number // sku价格
+    weight?: string | number // sku重量
+    skuDesc? : string // sku描述
+    skuAttrValueList?: Attr[]
+    skuSaleAttrValueList?: saleArr[]
+    skuDefaultImg?: string // sku图片地址
+    isSale?: number // 控制商品的上架与下架
+    id?: number
+}
+
+// 获取sku接口返回的数据ts类型
+export interface SkuResponseData extends ResponseData {
+    data: {
+        records: SkuData[]
+        total: number
+        size: number
+        current: number
+        orders: []
+        optimizeCountSql: boolean
+        hitCount: boolean
+        countId: null
+        maxLimit: null 
+        searchCount: boolean
+        pages: number
+    }
+}
+
+export interface SkuInfoData extends ResponseData {
+    data: SkuData
+}
+
--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -1,9 +1,138 @@
 ﻿<template>
-  <div>
-    <h1>SKU管理</h1>
-  </div>
+    <el-card>
+        <el-table style="margin: 10px 0px;" :data="skuArr" border>
+            <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
+            <el-table-column label="名称" show-overflow-tooltip width="150px" prop="skuName"></el-table-column>
+            <el-table-column label="描述" show-overflow-tooltip width="150px" prop="skuDesc"></el-table-column>
+            <el-table-column label="图片" width="150px">
+                <template #="{ row, $index }">
+                    <img :src="row.skuDefaultImg" alt="" style="width: 100px;height: 100px;">
+                </template>
+            </el-table-column>
+            <el-table-column label="重量" width="150px" prop="weight"></el-table-column>
+            <el-table-column label="价格" width="150px" prop="price"></el-table-column>
+            <el-table-column label="操作" width="250px" fixed="right">
+                <template #="{ row, $index }">
+                    <el-button type="primary" size="small" :icon="row.isSale == 1 ? 'Bottom' : 'Top'"
+                        @click="updateSale(row)"></el-button>
+                    <el-button type="primary" size="small" icon="Edit" @click="updateSku"></el-button>
+                    <el-button type="primary" size="small" icon="InfoFilled" @click="findSku(row)"></el-button>
+                    <el-popconfirm :title="`你确定要删除${row.skuName}?`" width="200px" @confirm="removeSku(row.id)">
+                        <template #reference>
+                            <el-button type="primary" size="small" icon="Delete"></el-button>
+                        </template>
+                    </el-popconfirm>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination v-model:current-page="pageNo" v-model:page-size="pageSize" :page-sizes="[10, 20, 30, 40]"
+            :background="true" layout="prev, pager, next, jumper,->,sizes,total" :total="total" @current-change="getHasSku"
+            @size-change="handler" />
+    </el-card>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue';
+//引入请求
+import { reqSkuList, reqSaleSku, reqCancelSale, reqSkuInfo, reqRemoveSku } from '@/api/product/index'
+//引入ts类型
+import type { SkuResponseData, SkuData, SkuInfoData } from '@/api/product/type';
+import { ElMessage } from 'element-plus';
 
-<style scoped></style>
+// 数据
+let skuArr = ref<SkuData[]>([]);
+// 页码
+let pageNo = ref<number>(1);
+// 页大小
+let pageSize = ref<number>(10);
+// 总数
+let total = ref<number>(0);
+// 控制抽屉显示与隐藏字段
+let drawer = ref<boolean>(false);
+let skuInfo = ref<any>({});
+
+// 获取数据方法
+const getHasSku = async (page = 1) => {
+    pageNo.value = page;
+    let result: SkuResponseData = await reqSkuList(pageNo.value, pageSize.value);
+    if (result.code == 200) {
+        console.log('aaa',result);
+        total.value = result.data.total
+        skuArr.value = result.data.records;
+
+    }
+}
+// 组件挂载完就获取数据
+onMounted(() => {
+    getHasSku();
+})
+// 分页器页面数量变化重新获取数据
+const handler = (pageSizes: number) => {
+    getHasSku();
+}
+
+// 商品的上架与下架
+const updateSale = async (row: SkuData) => {
+    if (row.isSale == 1) {
+        // 下架操作
+        await reqCancelSale(row.id as number);
+        ElMessage({ type: 'success', message: '下架成功' });
+        // 重新获取当前页面数据
+        getHasSku(pageNo.value)
+    } else {
+        // 下架操作
+        await reqSaleSku(row.id as number);
+        ElMessage({ type: 'success', message: '上架成功' });
+        getHasSku(pageNo.value)
+    }
+}
+
+// 更新已有的SKU
+const updateSku = () => {
+    ElMessage({ type: 'success', message: '更新中....' })
+}
+
+// 查看商品详情
+const findSku = async (row: SkuData) => {
+    // 抽屉展示
+    drawer.value = true;
+    // 获取已有商品的详情
+    let result: SkuInfoData = await reqSkuInfo(row.id as number);
+    skuInfo.value = result.data;
+}
+
+// 删除某个已有的商品
+const removeSku = async (id: number) => {
+    let result: any = await reqRemoveSku(id);
+    if (result.code == 200) {
+        ElMessage({ type: 'success', message: '删除成功' });
+        getHasSku(skuArr.value.length > 1 ? pageNo.value : pageNo.value - 1);
+    } else {
+        ElMessage({ type: 'error', message: '系统数据不能删除' })
+    }
+}
+
+
+
+
+
+
+</script>
+
+<style scoped lang="scss">
+.el-carousel__item h3 {
+    color: #475669;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+    text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
+}
+</style>
